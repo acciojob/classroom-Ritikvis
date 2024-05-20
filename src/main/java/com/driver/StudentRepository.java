246bd7d1cd4928b0560e32c14deedec39da4fcb5
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-05-20 13:02:16 +0530

    commit

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,74 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        if(student!=null){
+            studentMap.put(student.getName(),student);
+        }
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        if(teacher!=null){
+            teacherMap.put(teacher.getName(),teacher);
+        }
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            if(!teacherStudentMapping.containsKey(teacher)) {
+                teacherStudentMapping.put(teacher, new LinkedList<>());
+            }
+            teacherStudentMapping.get(teacher).add(student);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        return studentMap.get(student);
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        return teacherMap.get(teacher);
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        return teacherStudentMapping.get(teacher);
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String> list = new LinkedList<>();
+        for (Map.Entry<String, Student> entry : studentMap.entrySet()) {
+            list.add(entry.getKey());
+        }
+        return list;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        teacherMap.remove(teacher);
+        for(String student:teacherStudentMapping.get(teacher)) {
+            if(studentMap.containsKey(student))
+                studentMap.remove(student);
+        }
+        teacherStudentMapping.remove(teacher);
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+
+        teacherMap.clear();
+        for(String teacher : teacherStudentMapping.keySet()) {
+            for(String student:teacherStudentMapping.get(teacher)) {
+                if(studentMap.containsKey(student))
+                    studentMap.remove(student);
+            }
+
+        }
+        teacherStudentMapping.clear();
     }
 }
